50339a47d941ad2d779c216deb14561f9a274352
planeflight <olivier.lvannier@gmail.com>
2024-09-12 15:21:17 -0700

    tiny markview update

--- .config/nvim/lua/planeflight/plugins/init.lua
+++ .config/nvim/lua/planeflight/plugins/init.lua
@@ -144,7 +144,9 @@ return {
 
   {
     "OXY2DEV/markview.nvim",
+    lazy = false,
     dependencies = {
+      "nvim-treesitter/nvim-treesitter",
       "nvim-tree/nvim-web-devicons", -- Used by the code bloxks
     },
 
